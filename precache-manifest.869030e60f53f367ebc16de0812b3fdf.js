2415563bef7abeba4275b5283470fbec11ad4d52
zaiyangzhong <levizhong66@gmail.com>
2025-01-02 18:21:36 -0500

    Updates

---
+++ precache-manifest.869030e60f53f367ebc16de0812b3fdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56b90b2862c031191b13",
+    "url": "/levysite/static/css/main.043f907c.chunk.css"
+  },
+  {
+    "revision": "56b90b2862c031191b13",
+    "url": "/levysite/static/js/main.56b90b28.chunk.js"
+  },
+  {
+    "revision": "22dbe75e615c9bf60a43",
+    "url": "/levysite/static/js/runtime~main.22dbe75e.js"
+  },
+  {
+    "revision": "01aa700244b73aef9ca7",
+    "url": "/levysite/static/js/2.01aa7002.chunk.js"
+  },
+  {
+    "revision": "b4ff4f32b423f3cb2a8feec2559aec2c",
+    "url": "/levysite/index.html"
+  }
+];
